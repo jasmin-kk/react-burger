38f2a74a793462e614d60bffb3c69d900de1332e
Jasmin <k.jasmine@bestprofi.com>
2024-12-01 23:39:21 +0600

    fix modal

--- src/components/order-details/order-details.tsx
+++ src/components/order-details/order-details.tsx
@@ -1,118 +1,153 @@
-import React, { FC } from 'react';
+import React, { FC, useEffect, useState } from 'react';
+import { useSelector, useDispatch } from 'react-redux';
+import { fetchOrders } from '../../services/feed-service';
 import style from './order-details.module.css';
 import {
   CurrencyIcon,
   FormattedDate,
 } from '@ya.praktikum/react-developer-burger-ui-components';
+import { AppDispatch } from '../../store';
 
 export const OrderDetails: FC = () => {
+  const dispatch: AppDispatch = useDispatch();
   const today = new Date();
+
+  const [order, setOrder] = useState<any>(null);
+
+  const orders = useSelector((state: any) => state.orders.orders);
+  const { ingredients, error } = useSelector((state: any) => state.ingredients);
+
+  const pathParts = window.location.pathname.split('/');
+  const orderId = pathParts[pathParts.length - 1];
+
+  useEffect(() => {
+    dispatch(fetchOrders());
+  }, [dispatch]);
+
+  useEffect(() => {
+    if (orders.length > 0) {
+      const foundOrder = orders.find((order: any) => order._id === orderId);
+      if (foundOrder) {
+        setOrder(foundOrder);
+        console.log(foundOrder, 'foundOrder');
+      }
+    }
+  }, [orders, orderId]);
+
+  const getStatusTextAndColor = (status: string) => {
+    if (status === 'done') {
+      return { text: 'Выполнен', color: '#00CCCC' };
+    } else if (status === 'created') {
+      return { text: 'Создан', color: 'white' };
+    } else {
+      return { text: 'Готовится', color: 'white' };
+    }
+  };
+
+  const { text: statusText, color: statusColor } = getStatusTextAndColor(
+    order.status
+  );
+
+  const orderIngredients = order
+    ? order.ingredients.map((ingredientId: string) =>
+        ingredients.find((ingredient: any) => ingredient._id === ingredientId)
+      )
+    : [];
+
+  const groupedIngredients = orderIngredients.reduce(
+    (acc: any, ingredient: any) => {
+      if (ingredient) {
+        if (acc[ingredient._id]) {
+          acc[ingredient._id].count += 1;
+          acc[ingredient._id].totalPrice += ingredient.price;
+        } else {
+          acc[ingredient._id] = {
+            ingredient,
+            count: 1,
+            totalPrice: ingredient.price,
+          };
+        }
+      }
+      return acc;
+    },
+    {}
+  );
+
+  const groupedIngredientsArray = Object.values(groupedIngredients);
+
+  const totalPrice = groupedIngredientsArray.reduce(
+    (sum: number, { totalPrice }: any) => {
+      return sum + totalPrice;
+    },
+    0
+  );
+
   return (
     <div className={style.main}>
-      <p className={`text text_type_digits-default mb-8 ${style.center}`}>
-        #034533
-      </p>
-      <p className="text text_type_main-medium mb-3">
-        Black Hole Singularity острый бургер
-      </p>
-      <p className={`text text_type_main-default ${style.color} mb-15`}>
-        Выполнен
-      </p>
-      <p className="text text_type_main-medium mb-6">Состав:</p>
-      <div className={style.scroll}>
-        <div className={style.block}>
-          <div
-            className={`mr-4 ${style.img}`}
-            style={{
-              backgroundImage:
-                "url('https://code.s3.yandex.net/react/code/bun-02.png')",
-            }}
-          />
-          <div className={style.end}>
-            <p className="text text_type_main-default">
-              Флюоресцентная булка R2-D3
-            </p>
-            <div className={style.elementSum}>
-              <p className="text text_type_digits-default mr-2">2 x 20</p>
-              <CurrencyIcon type="primary" />
-            </div>
-          </div>
-        </div>
-        <div className={style.block}>
-          <div
-            className={`mr-4 ${style.img}`}
-            style={{
-              backgroundImage:
-                "url('https://code.s3.yandex.net/react/code/bun-02.png')",
-            }}
-          />
-          <div className={style.end}>
-            <p className="text text_type_main-default">
-              Флюоресцентная булка R2-D3
-            </p>
-            <div className={style.elementSum}>
-              <p className="text text_type_digits-default mr-2">2 x 20</p>
-              <CurrencyIcon type="primary" />
-            </div>
-          </div>
-        </div>
-        <div className={style.block}>
-          <div
-            className={`mr-4 ${style.img}`}
-            style={{
-              backgroundImage:
-                "url('https://code.s3.yandex.net/react/code/bun-02.png')",
-            }}
-          />
-          <div className={style.end}>
-            <p className="text text_type_main-default">
-              Флюоресцентная булка R2-D3
-            </p>
-            <div className={style.elementSum}>
-              <p className="text text_type_digits-default mr-2">2 x 20</p>
-              <CurrencyIcon type="primary" />
-            </div>
-          </div>
-        </div>
-        <div className={style.block}>
-          <div
-            className={`mr-4 ${style.img}`}
-            style={{
-              backgroundImage:
-                "url('https://code.s3.yandex.net/react/code/bun-02.png')",
-            }}
-          />
-          <div className={style.end}>
-            <p className="text text_type_main-default">
-              Флюоресцентная булка R2-D3
-            </p>
-            <div className={style.elementSum}>
-              <p className="text text_type_digits-default mr-2">2 x 20</p>
-              <CurrencyIcon type="primary" />
-            </div>
-          </div>
-        </div>
-      </div>
-      <div className={style.footer}>
-        <span className={style.date}>
-          <FormattedDate
-            date={
-              new Date(
-                today.getFullYear(),
-                today.getMonth(),
-                today.getDate(),
-                today.getHours(),
-                today.getMinutes() - 1,
-                0
+      {order ? (
+        <>
+          <p className={`text text_type_digits-default mb-8 ${style.center}`}>
+            #{order.number}
+          </p>
+          <p className="text text_type_main-medium mb-3">
+            {order.name} острый бургер
+          </p>
+          <p
+            className={`text text_type_main-default ${style.color} mb-15`}
+            style={{ color: statusColor }}
+          >
+            {statusText}
+          </p>
+          <p className="text text_type_main-medium mb-6">Состав:</p>
+          <div className={style.scroll}>
+            {groupedIngredientsArray.map(
+              ({ ingredient, count, totalPrice }: any, index: number) => (
+                <div className={style.block} key={index}>
+                  <div
+                    className={style.img}
+                    style={{
+                      backgroundImage: `url(${ingredient.image})`, // Use the ingredient image from Redux
+                    }}
+                  />
+                  <div className={style.end}>
+                    <p className="text text_type_main-default">
+                      {ingredient.name}
+                    </p>
+                    <div className={style.elementSum}>
+                      <p className="text text_type_digits-default mr-2">
+                        {count} x {ingredient.price}
+                      </p>
+                      <CurrencyIcon type="primary" />
+                    </div>
+                  </div>
+                </div>
               )
-            }
-          />
-        </span>
-        <div className={style.total}>
-          <p className=" text text_type_digits-default mr-2">510</p>
-          <CurrencyIcon type="primary" />
-        </div>
-      </div>
+            )}
+          </div>
+          <div className={style.footer}>
+            <span className={style.date}>
+              <FormattedDate
+                date={
+                  new Date(
+                    today.getFullYear(),
+                    today.getMonth(),
+                    today.getDate(),
+                    today.getHours(),
+                    today.getMinutes() - 1,
+                    0
+                  )
+                }
+              />
+            </span>
+            <div className={style.total}>
+              <p className="text text_type_digits-default mr-2">{totalPrice}</p>
+              <CurrencyIcon type="primary" />
+            </div>
+          </div>
+        </>
+      ) : (
+        <p>Loading...</p>
+      )}
     </div>
   );
 };

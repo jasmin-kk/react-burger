2ff9e1047a391e11d0aacdcfa70d5cabbf62f48c
Jasmin <k.jasmine@bestprofi.com>
2024-12-01 19:36:47 +0600

    fix total sum

--- src/components/info/info.tsx
+++ src/components/info/info.tsx
@@ -71,7 +71,6 @@ export const Info: FC<InfoProps> = ({ orders, total, totalToday }) => {
         </div>
       </div>
 
-      {/* Общая информация о выполненных заказах */}
       <p className="text text_type_main-medium">Выполнено за все время:</p>
       <p className="text text_type_digits-large mb-15">{total}</p>
       <p className="text text_type_main-medium">Выполнено за сегодня:</p>
